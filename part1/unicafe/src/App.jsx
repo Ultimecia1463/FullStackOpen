e6fec1d5b19364041041d714b83d7a02b82c904e
Ansh_sharma <keshu1234harshu@gmail.com>
2025-04-30 23:09:17 +0530

    1.9 added

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,7 +1,8 @@
 import { useState } from 'react'
 
-const Statistics = ({good,neutral,bad}) => {
+const Statistics = ({good,neutral,bad,feedback}) => {
   const total = good+neutral+bad
+  if(!feedback){ return <p>No feedback given</p>}
   return (
     <>
       <p>good {good} </p>
@@ -19,18 +20,30 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [feedback,setFeedback] = useState(false) 
 
   return (
     <>
       <h1>give feedback</h1>
 
-      <button onClick={()=>setGood(good+1)} >good</button>
-      <button onClick={()=>setNeutral(neutral+1)} >neutral</button>
-      <button onClick={()=>setBad(bad+1)} >bad</button>
+      <button onClick={()=>{
+        setGood(good+1)
+        setFeedback(true)
+      }} >good</button>
+
+      <button onClick={()=>{
+        setNeutral(neutral+1)
+        setFeedback(true)
+      }} >neutral</button>
+
+      <button onClick={()=>{
+        setBad(bad+1)
+        setFeedback(true)
+      }} >bad</button>
 
       <h1>statistics</h1>
       
-      <Statistics good={good} neutral={neutral} bad={bad}/>
+      <Statistics good={good} neutral={neutral} bad={bad} feedback={feedback} />
     </>
   )
 }
