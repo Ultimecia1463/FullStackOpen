1572a219933926a256e716fac300c59dee08f5c5
Ansh_sharma <keshu1234harshu@gmail.com>
2025-05-01 14:59:51 +0530

    1.11 added

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,53 +1,50 @@
 import { useState } from 'react'
 
-const Statistics = ({good,neutral,bad,feedback}) => {
+const Statistics = ({good,neutral,bad}) => {
   const total = good+neutral+bad
-  if(!feedback){ return <p>No feedback given</p>}
+  if(total===0){ return <p>No feedback given</p>}
   return (
-    <>
-      <StatisticLine text="good" stat={good} />
-      <StatisticLine text="neutral" stat={neutral} />
-      <StatisticLine text="bad" stat={bad} />
-      <StatisticLine text="all" stat={total} />
-      <StatisticLine text="average" stat={total/3} />
-      <StatisticLine text="positive" stat={good/total*100} />g
-    </>
+    <table>
+      <tbody>
+        <StatisticLine text="good" stat={good} />
+        <StatisticLine text="neutral" stat={neutral} />
+        <StatisticLine text="bad" stat={bad} />
+        <StatisticLine text="all" stat={total} />
+        <StatisticLine text="average" stat={total/3} />
+        <StatisticLine text="positive" stat={good/total*100} />
+      </tbody>
+    </table>
   )
 }
 
 const Button = ({onClick,text})=> <button onClick={onClick} >{text}</button>
 
-const StatisticLine = ({text,stat}) => <p>{text} {stat}</p>
+const StatisticLine = ({text,stat}) => {
+  return (
+    <tr>
+      <td> {text} </td>
+      <td> {stat} </td>
+    </tr>
+  )
+}
 
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-  const [feedback,setFeedback] = useState(false) 
 
   return (
     <>
       <h1>give feedback</h1>
 
-      <Button onClick={ ()=> {
-        setGood(good+1)
-        setFeedback(true)
-      } } text="good" />
-
-      <Button onClick={ ()=> {
-        setNeutral(neutral+1)
-        setFeedback(true)
-      } } text="neutral" />
-
-      <Button onClick={ ()=> {
-        setBad(bad+1)
-        setFeedback(true)
-      } } text="bad" />
+      <Button onClick={ ()=> {setGood(good+1)} } text="good" />
+      <Button onClick={ ()=> {setNeutral(neutral+1)} } text="neutral" />
+      <Button onClick={ ()=> {setBad(bad+1)} } text="bad" />
 
       <h1>statistics</h1>
       
-      <Statistics good={good} neutral={neutral} bad={bad} feedback={feedback} />
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </>
   )
 }
