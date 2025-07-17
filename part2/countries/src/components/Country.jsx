8c880720fe1f89b0973383ffd495fa2e55688d60
Ansh_sharma <keshu1234harshu@gmail.com>
2025-07-18 01:00:13 +0530

    add 2.20

--- part2/countries/src/components/Country.jsx
+++ part2/countries/src/components/Country.jsx
@@ -1,6 +1,25 @@
-import React from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 const Country = ({country}) => {
+    const apikey = import.meta.env.VITE_WEATHER_API_KEY
+    const url = `https://api.weatherapi.com/v1/current.json?key=${apikey}&q=${country.capital}`
+    const [weather, setWeather] = useState(null)
+
+    useEffect(()=>{
+        axios
+            .get(url)
+            .then(res=>{
+                const trimmed = {
+                    conditionIcon: res.data.current.condition.icon,
+                    windSpeed: res.data.current.wind_kph,
+                    temperature: res.data.current.heatindex_c
+                }
+                setWeather(trimmed)
+            })
+            .catch(e=>console.log(e.message))
+    },[country])
+    
   return (
     <>
         <h1> {country.name} </h1>
@@ -13,6 +32,14 @@ const Country = ({country}) => {
             ))}
         </ ul>
         <img src={country.flag} alt="Country flag"></img>
+        {weather && (
+            <>
+                <h2>Weather in {country.capital}</h2>
+                <p>Temperature: {weather.temperature} °C</p>
+                <img src={weather.conditionIcon} alt="Weather condition" />
+                <p>Wind: {weather.windSpeed} km/h</p>
+            </>
+        )}
     </>
   )
 }
