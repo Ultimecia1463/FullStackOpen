1f3ac4ac4b60d13a1e1c496c66a5122b363fbc20
Ansh_sharma <keshu1234harshu@gmail.com>
2025-07-20 00:34:13 +0530

    add 3.2

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -28,6 +28,11 @@ app.get('/', (request, response) => {
   response.send('<h1>Phonebook</h1>')
 })
 
+app.get('/info', (request, response) => {
+  response.send(`<p>Phonebook has info for ${persons.length} people</p>` +
+               `<p>${new Date()}</p>`)
+})
+
 app.get('/api/persons', (request, response) => {
   response.json(persons)
 })
