ba46807eba2c8b2252467449c772e46e4586e203
Ansh_sharma <keshu1234harshu@gmail.com>
2025-07-20 01:08:44 +0530

    add 3.6

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -70,6 +70,12 @@ app.post('/api/persons', (request, response) => {
     })
   }
 
+  if(persons.some(person => person.name === body.name)) {
+    return response.status(409).json({
+      error: 'name must be unique',
+    })
+  }
+
   const person = {
     name: body.name,
     number: body.number,
