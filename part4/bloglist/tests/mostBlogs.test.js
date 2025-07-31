57c44f3671d756ab9feb8e01539b86f23b828537
Ansh_sharma <keshu1234harshu@gmail.com>
2025-07-31 18:49:06 +0530

    add add 4.6

---
+++ part4/bloglist/tests/mostBlogs.test.js
@@ -0,0 +1,46 @@
+const { test, describe } = require('node:test')
+const assert = require('node:assert')
+const listHelper = require('../utils/list_helper')
+
+describe('Most active author', () => {
+	const Blogs = [
+		{
+			title: 'React patterns',
+			author: 'Michael Chan',
+			likes: 7,
+		},
+		{
+			title: 'Go To Statement Considered Harmful',
+			author: 'Edsger W. Dijkstra',
+			likes: 5,
+		},
+		{
+			title: 'Canonical string reduction',
+			author: 'Edsger W. Dijkstra',
+			likes: 12,
+		},
+		{
+			title: 'First class tests',
+			author: 'Robert C. Martin',
+			likes: 10,
+		},
+		{
+			title: 'TDD harms architecture',
+			author: 'Robert C. Martin',
+			likes: 0,
+		},
+		{
+			title: 'Type wars',
+			author: 'Robert C. Martin',
+			likes: 12,
+		}  
+	]
+    
+	test('The author with most blogs', () => {
+		const result = listHelper.mostBlogs(Blogs)
+    assert.deepStrictEqual(result, {
+      author: 'Robert C. Martin',
+      count: 3
+    })
+	})
+})
--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -1,3 +1,5 @@
+const _ = require('lodash')
+
 const dummy = (blogs) => {
   return 1
 }
@@ -13,8 +15,20 @@ const favoriteBlog = (blogs) => {
   return blogs.reduce((max, blog) => (blog.likes > max.likes ? blog : max), blogs[0])
 }
 
+const mostBlogs = (blogs) => {
+  const authorCount = _.countBy(blogs, 'author')
+  
+  return _.reduce(authorCount, (max, count, author)=> {
+    if (count > max.count) {
+      return { author, count }
+    }
+    return max
+  },{ author: '', count: 0 })
+}
+
 module.exports = {
   dummy,
   totalLikes,
   favoriteBlog,
+  mostBlogs
 }
