f621dd7ed52a90a700db6710d87c139f54652f49
Ansh_sharma <keshu1234harshu@gmail.com>
2025-08-13 19:00:18 +0530

    add 4.15

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -4,6 +4,7 @@ const config = require('./utils/config')
 const logger = require('./utils/logger')
 const middleware = require('./utils/middleware')
 const notesRouter = require('./controllers/blogs')
+const usersRouter = require('./controllers/users')
 
 const app = express()
 
@@ -23,6 +24,7 @@ app.use(express.json())
 app.use(middleware.requestLogger)
 
 app.use('/api/blogs', notesRouter)
+app.use('/api/users', usersRouter)
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
---
+++ part4/bloglist/controllers/users.js
@@ -0,0 +1,23 @@
+const bcrypt = require('bcrypt')
+const User = require('../models/user')
+const usersRouter = require('express').Router()
+
+usersRouter.post('/', async (request, response) => {
+  const { username, name, password } = request.body
+  const passwordHash = await bcrypt.hash(password, 10)
+  const user = new User({
+    username,
+    name,
+    passwordHash
+  })
+
+  const savedUser = await user.save()
+  response.status(201).json(savedUser)
+})
+
+usersRouter.get('/', async (request, response) => {
+  const users = await User.find({}).populate('blogs', { title: 1, author: 1, url: 1, likes: 1 })
+  response.json(users)
+})
+
+module.exports = usersRouter
