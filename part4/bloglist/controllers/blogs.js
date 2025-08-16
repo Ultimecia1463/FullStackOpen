b495a63e59b23b5e13b797a0bab1b0381ac5d8b3
Ansh_sharma <144516349+Ultimecia1463@users.noreply.github.com>
2025-08-16 20:57:15 +0530

    add 4.20

--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -4,21 +4,13 @@ const User = require('../models/user')
 const jwt = require('jsonwebtoken')
 const config = require('../utils/config')
 
-const getTokenFrom = request => {
-  const authorization = request.get('authorization')
-  if (authorization && authorization.startsWith('Bearer ')) {
-    return authorization.replace('Bearer ', '')
-  }
-  return null
-}
-
 blogsRouter.get('/', async (request, response, next) => {
   const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 })
   response.json(blogs)
 })
 
 blogsRouter.post('/', async (request, response, next) => {
-  const decodedToken = jwt.verify(getTokenFrom(request), config.JWT_SECRET)
+  const decodedToken = jwt.verify(request.token, config.JWT_SECRET)
   if (!decodedToken.id) {
     return response.status(401).json({ error: 'token invalid' })
   }
