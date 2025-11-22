2fea1bae917760398faad968c3b17ec5feb272c2
Ansh_sharma <144516349+Ultimecia1463@users.noreply.github.com>
2025-11-22 22:30:59 +0530

    add 5.14

--- part5/bloglist-frontend/src/components/Blog.test.jsx
+++ part5/bloglist-frontend/src/components/Blog.test.jsx
@@ -1,17 +1,22 @@
-import { render } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import Blog from './Blog'
 
 describe('<Blog />', () => {
   let container
 
   beforeEach(() => {
+    const user = {
+      username: 'This is username'
+    }
     const blog = {
       title: 'This is title',
       author: 'This is author',
       url: 'This is url',
-      likes: 114514
+      likes: 114514,
+      user: { name: 'This is user.name' }
     }
-    container = render(<Blog blog={blog} />).container
+    container = render(<Blog user={user} blog={blog} />).container
   })
 
   test('Blog renders title and author by default', async () => {
@@ -20,9 +25,19 @@ describe('<Blog />', () => {
     expect(div).toHaveTextContent('This is author')
   })
 
-  test('Blog does not renders url and lilkes by default', async () => {
+  test('Blog does not renders url and likes by default', async () => {
     const div = container.querySelector('.blog')
     expect(div).not.toHaveTextContent('This is url')
     expect(div).not.toHaveTextContent(114514)
+  })
+
+  test('Blog renders url and likes when button is clicked', async () => {
+    const user = userEvent.setup()
+    const button = screen.getByText('view')
+    await user.click(button)
+
+    const div = container.querySelector('.blog')
+    expect(div).toHaveTextContent('This is url')
+    expect(div).toHaveTextContent(114514)
   })
 })
